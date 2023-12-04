f4677f19de5b312d651310c3052b427caf3ff107
Toby Bentall <tobybentall@hotmail.co.uk>
2023-12-04 20:48:04 +0000

    Add deplpyed allowed host

--- pokerblog/settings.py
+++ pokerblog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-t0bes1-blogpoker-n5mimwy9bv6.ws-eu106.gitpod.io', 'blog-poker.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-t0bes1-blogpoker-n5mimwy9bv6.ws-eu106.gitpod.io', 'blog-poker-2e9e23ce2538.herokuapp.com', 'localhost']
 
 
 # Application definition
