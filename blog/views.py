c2ab6f84e9fff9bdcf5713c88bdc58c7b52db190
Toby Bentall <tobybentall@hotmail.co.uk>
2023-12-07 14:00:02 +0000

    Add view logic

--- blog/views.py
+++ blog/views.py
@@ -11,10 +11,18 @@ class PostList(generic.ListView):
 def post_summary(request):
     """View function for home page of site."""
 
-    mydata = Post.objects.values_list('excerpt')
+    mydata = Post.objects.all()
+    mydata1 = list(mydata)
+    title = mydata.values_list('title')
+    created_on = mydata.values_list('created_on')
+    excerpt = mydata.values_list('excerpt')
+    
 
     context = {
-        'data_dump': mydata,
+        'data_dump': mydata1,
+        'excerpt': excerpt,
+        'title': title,
+        'created_on': created_on,
     }
 
     # Render the HTML template index.html with the data in the context variable
