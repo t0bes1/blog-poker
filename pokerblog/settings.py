c7cdad5c1e2a0b9056a7423e10479e2894f61d29
Toby Bentall <tobybentall@hotmail.co.uk>
2024-01-27 23:03:58 +0000

    Install whitenoise

--- pokerblog/settings.py
+++ pokerblog/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pokerblog.urls'
