9a552ae494d251c11765916f25d63e31d20a8339
Toby Bentall <tobybentall@hotmail.co.uk>
2024-01-28 00:06:01 +0000

    Remove media files

--- pokerblog/settings.py
+++ pokerblog/settings.py
@@ -148,9 +148,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
